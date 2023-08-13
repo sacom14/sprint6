29617731c31b6ea4472404cdcb477319a2fd4fe7
sacom1441 <samir.comas1@gmail.com>
2023-08-13 19:20:03 +0200

    sprint6 nivell1 exercici1

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-escena',
+  selector: 'app-home-escena',
   templateUrl: './escena.component.html',
   styleUrls: ['./escena.component.css']
 })
 export class EscenaComponent {
-
+  public frase:string = "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial";
 }
