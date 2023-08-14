51fef88f999fc847498096f477a175449820e751
sacom1441 <samir.comas1@gmail.com>
2023-08-14 14:45:28 +0200

    sprint6 nivell1 exercici4

--- src/app/escena/escena.component.ts
+++ src/app/escena/escena.component.ts
@@ -10,8 +10,20 @@ import { TextArrayService } from '../services/text.service';
 })
 export class EscenaComponent {
 // public frase="El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial";
-constructor(public storie:TextArrayService){
-  
+constructor(public storie:TextArrayService){}
+
+public currentSentence:number =0;
+
+prev():void{
+  if (this.currentSentence > 0){
+    this.currentSentence--;
+  }
+}
+
+next():void{
+  if (this.currentSentence < this.storie.storyArrayText.length-1){
+    this.currentSentence++;
+  }
 }
 
 }
